636491df43aface2847bfdfe5f5cacf0a7e904bb
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-08-12 16:58:39 +0900

    선택정렬 주석 추가

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -10,18 +10,18 @@ public class SelectionSort {
     }
 
     private static void selectionSort(int[] array) {
-        int min;
-        int temp;
+        int minIndex; // 가장 작은 원소의 인덱스
+        int temp; // 데이터 swap용 임시변수
 
-        for(int i = 0; i<array.length-1; i++){
-            min = i;
-            for(int j = i+1; j<array.length; j++){
-                if (array[min]>array[j]) {
-                    min = j;
+        for(int i = 0; i<array.length-1; i++){  // 마지막 요소는 자동 정렬됨으로  array.length-1까지 반복
+            minIndex = i;
+            for(int j = i+1; j<array.length; j++){  // 최솟값 찾기
+                if (array[minIndex]>array[j]) {
+                    minIndex = j;   // 현재 가장 작은 값보다 작으면 갱신
                 }
             }
-            temp = array[min];
-            array[min] = array[i];
+            temp = array[minIndex];
+            array[minIndex] = array[i];
             array[i] = temp;
 
         }
