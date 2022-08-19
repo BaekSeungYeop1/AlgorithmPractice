80158b58ff8e4af11496896d1f323058bbd4f575
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-08-19 23:42:27 +0900

    선택 정렬(최대값 기준) 추가

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -4,11 +4,12 @@ public class SelectionSort {
     public static void main(String args[]){
         int[] array = {1,10,5,8,7,6,4,3,2,9};
 
-        selectionSort(array);
+        selectionSort2(array);
 
         System.out.println(Arrays.toString(array));
     }
 
+    // 최소값
     private static void selectionSort(int[] array) {
         int minIndex; // 가장 작은 원소의 인덱스
         int temp; // 데이터 swap용 임시변수
@@ -24,6 +25,23 @@ public class SelectionSort {
             array[minIndex] = array[i];
             array[i] = temp;
 
+        }
+    }
+    // 최대값
+    private static void selectionSort2(int[] array){
+        int maxIndex;
+        int temp;
+
+        for(int i = array.length-1; i>0; i--){
+            maxIndex = i;
+            for (int j = i-1; j>=0; j--){
+                if(array[maxIndex]<array[j]){
+                    maxIndex = j;
+                }
+            }
+            temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
         }
     }
 }
